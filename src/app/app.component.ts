07806a286b281ec7937ad157b05f61e650834daa
Alfredo <gafaalfredo02@gmail.com>
2024-02-28 22:39:41 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Dados-JAGF';
+  dadoIzquierdo='../assets/images/dice2.png';
+  dadoDerecho='../assets/images/dice2.png';
+  numero1: number=1;
+  numero2: number=2;
+
+  tirarDados():void{
+    this.numero1=Math.round(Math.random()*5)+1;
+    this.numero2=Math.round(Math.random()*5)+1;
+
+    this.dadoIzquierdo='../assets/images/dice'+this.numero1+'.png';
+    this.dadoDerecho='../assets/images/dice'+this.numero2+'.png';
+    console.log(this.numero1,this.numero2);
+  }  
 }
